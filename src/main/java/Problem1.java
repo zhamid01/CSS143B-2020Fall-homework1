86252d4464928140692c9b532b98f49ad5ef1f2d
zhamid01 <zhamid01@uw.edu>
2020-10-07 23:31:03 -0700

    Returning -1

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -2,7 +2,9 @@ public class Problem1 {
 
     // Do not change signature (function name, parameters)
     public static int binarySearch(int[] data, int target) {
-        // Add your solution
-        return -1; // Place holder
+        if (data == null || data.length == 0) {
+            return -1;
+        }
+        return -1;
     }
 }
