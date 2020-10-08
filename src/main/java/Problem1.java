8d55e3ac8e5a409fbe85e1b7ea91728fc90c2d2a
zhamid01 <zhamid01@uw.edu>
2020-10-08 01:28:32 -0700

    Adding Comments

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -1,7 +1,20 @@
 public class Problem1 {
 
+    /*
+    Name: Zayeem Hamid
+    Class: CSS143 B
+    Date: October 8th, 2020
+
+    These sites were used as help:
+    https://www.geeksforgeeks.org/binary-search/
+    https://www.tutorialspoint.com/data_structures_algorithms/binary_search_algorithm.htm
+    */
+
     // Do not change signature (function name, parameters)
     public static int binarySearch(int[] data, int target) {
+        if (data == null) {
+            return -1;
+        }
         int s = 0;
         int length = data.length - 1;
 
