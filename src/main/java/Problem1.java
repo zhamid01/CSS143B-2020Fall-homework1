8f546b6870b86d53fbc6014b4dc13fba6f526c9f
zhamid01 <zhamid01@uw.edu>
2020-10-07 23:36:40 -0700

    Adding null and array length 0

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -5,20 +5,16 @@ public class Problem1 {
         if (data == null || data.length == 0) {
             return -1;
         }
-
         int mid = (data.length) / 2;
-        while (mid < data.length)
+        while (mid <= data.length - 1) {
             if (data[mid] == target) {
                 return mid;
-            }
-
-            else if (data[mid] > target) {
+            } else if (data[mid] > target) {
                 mid++;
-            }
-
-            else if (data[mid] < target) {
+            } else if (data[mid] < target) {
                 mid--;
             }
+        }
         return -1;
     }
 }
