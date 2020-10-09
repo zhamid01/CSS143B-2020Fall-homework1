9eb7d6489a506cb894cd60f1188c76a37d51fe2b
zhamid01 <zhamid01@uw.edu>
2020-10-08 18:53:31 -0700

    Adding another test

--- src/test/java/Problem2Test.java
+++ src/test/java/Problem2Test.java
@@ -10,11 +10,13 @@ public class Problem2Test {
     public void testBubbleSort() {
         int inputs[][] = {
                 {91, 21, 19, 22, 25},
-                {6,2,11,1,4,19}
+                {6,2,11,1,4,19},
+                {6,12,9,2,8,4,3,11,22,1}
         };
         int answer[][] = {
                 {19, 21, 22, 25, 91},
-                {1,2,4,6,11,19}
+                {1,2,4,6,11,19},
+                {1,2,3,4,6,8,9,11,12,22}
         };
         for (int i = 0; i < inputs.length; i++) {
             Problem2.bubbleSort(inputs[i]);
