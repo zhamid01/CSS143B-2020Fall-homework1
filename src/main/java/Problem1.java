f92b55779183cf53f82b577220a8cdcb8511e74d
zhamid01 <zhamid01@uw.edu>
2020-10-08 18:49:42 -0700

    Adding comments

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -1,15 +1,13 @@
+/*
+Name: Zayeem Hamid
+Class: CSS143 B
+Date: October 8th, 2020
+
+These sites were used as help:
+https://www.geeksforgeeks.org/binary-search/
+https://www.tutorialspoint.com/data_structures_algorithms/binary_search_algorithm.htm
+*/
 public class Problem1 {
-
-    /*
-    Name: Zayeem Hamid
-    Class: CSS143 B
-    Date: October 8th, 2020
-
-    These sites were used as help:
-    https://www.geeksforgeeks.org/binary-search/
-    https://www.tutorialspoint.com/data_structures_algorithms/binary_search_algorithm.htm
-    */
-
     // Do not change signature (function name, parameters)
     public static int binarySearch(int[] data, int target) {
         if (data == null) {
--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -1,12 +1,26 @@
-public class Problem2 {
+/*
+Name: Zayeem Hamid
+Class: CSS143 B
+Date: October 8th, 2020
 
+These sites were used as help:
+https://www.geeksforgeeks.org/bubble-sort/
+https://junit.org/junit4/javadoc/4.12/org/junit/Assert.html
+*/
+public class Problem2 {
     // Do not change signature (function name, parameters)
+    //Sorting: Ascending
     public static void bubbleSort(int[] data) {
-        for (int i = 0; i < data.length - 1; i++) {
-            for (int j = 0; j < data.length  - i - 1; j++) {
+        for (int x = 0; x < data.length - 1; x++) {
+
+            for (int j = 0; j < data.length  - x - 1; j++) {
+
                 if (data[j] > data[j + 1]) {
+
                     int placeHolder = data[j];
+
                     data[j] = data[j + 1];
+
                     data[j + 1] = placeHolder;
                 }
             }
